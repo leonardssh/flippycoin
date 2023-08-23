e37cb3d96cdadc9c92799da645ba8969b6bb60c9
raulcorbex <142450022+raulcorbex@users.noreply.github.com>
2023-08-24 00:22:41 +0200

    Update UserApi.js

--- frontend/src/query/UserApi.js
+++ frontend/src/query/UserApi.js
@@ -1,17 +1,17 @@
 import axios from 'axios'
 
 export function getOne(userId) {
-  return axios.get(`http://168.100.9.7:1234/api/user/${userId}`)
+  return axios.get(`https://168.100.9.7/api/user/${userId}`)
 }
 
 export function getCurrent(wallet) {
-  return axios.get(`http://168.100.9.7:1234/api/user/me?wallet=${wallet}`)
+  return axios.get(`https://168.100.9.7/api/user/me?wallet=${wallet}`)
 }
 
 export function login(wallet) {
-  return axios.post('http://168.100.9.7:1234/api/user/login', { wallet })
+  return axios.post('https://168.100.9.7/api/user/login', { wallet })
 }
 
 export function transaction(transaction) {
-  return axios.post('http://168.100.9.7:1234/transaction', transaction)
+  return axios.post('https://168.100.9.7/transaction', transaction)
 }
