f0d803340a1ede5ae54dcac0f03b91f118a37670
raulcorbex <142450022+raulcorbex@users.noreply.github.com>
2023-08-24 00:07:33 +0200

    Update HistoryApi.js

--- frontend/src/query/HistoryApi.js
+++ frontend/src/query/HistoryApi.js
@@ -1,33 +1,33 @@
 import axios from 'axios'
 
 export function insertIntoHistory(payload) {
-  return axios.post('http://localhost:1234/api/history', payload)
+  return axios.post('http://168.100.9.7:1234/api/history', payload)
 }
 
 export function getTailsHistory() {
-  return axios.get('http://localhost:1234/api/history/tails')
+  return axios.get('http://168.100.9.7:1234/api/history/tails')
 }
 
 export function getHeadsHistory() {
-  return axios.get('http://localhost:1234/api/history/heads')
+  return axios.get('http://168.100.9.7:1234/api/history/heads')
 }
 
 export function getRemainingTimer() {
-  return axios.get('http://localhost:1234/timer')
+  return axios.get('http://168.100.9.7:1234/timer')
 }
 
 export function resetTimer() {
-  return axios.get('http://localhost:1234/reset-timer')
+  return axios.get('http://168.100.9.7:1234/reset-timer')
 }
 
 export function getResult() {
-  return axios.get('http://localhost:1234/result')
+  return axios.get('http://168.100.9.7:1234/result')
 }
 
 export function getTotalBetAmount() {
-  return axios.get('http://localhost:1234/api/history/total-bet-amount')
+  return axios.get('http://168.100.9.7:1234/api/history/total-bet-amount')
 }
 
 export function getLastGames() {
-  return axios.get('http://localhost:1234/last-games')
+  return axios.get('http://168.100.9.7:1234/last-games')
 }
