495cf534179daea7b162eca85e0478a8669be99c
raulcorbex <142450022+raulcorbex@users.noreply.github.com>
2023-08-25 19:34:19 +0200

    Update UserApi.js

--- frontend/src/query/UserApi.js
+++ frontend/src/query/UserApi.js
@@ -1,17 +1,17 @@
 import axios from 'axios'
 
 export function getOne(userId) {
-  return axios.get(`https://168.100.9.7/api/user/${userId}`)
+  return axios.get(`https://api.flippycoin.io/user/${userId}`)
 }
 
 export function getCurrent(wallet) {
-  return axios.get(`https://168.100.9.7/api/user/me?wallet=${wallet}`)
+  return axios.get(`https://api.flippycoin.io/user/me?wallet=${wallet}`)
 }
 
 export function login(wallet) {
-  return axios.post('https://168.100.9.7/api/user/login', { wallet })
+  return axios.post('https://api.flippycoin.io/user/login', { wallet })
 }
 
 export function transaction(transaction) {
-  return axios.post('https://168.100.9.7/transaction', transaction)
+  return axios.post('https://api.flippycoin.io/transaction', transaction)
 }
